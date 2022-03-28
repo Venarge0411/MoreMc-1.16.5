9578331dfdaaf547a3d3726fb064139b87b4cbfb
Venarge0411 <leonardojmarinellib09@gmail.com>
2022-03-28 08:12:06 -0400

    se murio el perro

--- src/main/java/net/more/moremc/item/ModItems.java
+++ src/main/java/net/more/moremc/item/ModItems.java
@@ -1,7 +1,12 @@
 package net.more.moremc.item;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.more.moremc.MoreMc;
@@ -11,8 +16,8 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create (ForgeRegistries.ITEMS, MoreMc.MOD_ID);
 
-
     public static void register(IEventBus eventBus){
         ITEMS.register (eventBus);
     }
+
 }
---
+++ src/main/java/net/more/moremc/item/custom/ender_ballest.java
@@ -0,0 +1,45 @@
+package net.more.moremc.item.custom;
+
+import net.minecraft.entity.item.EnderPearlEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.EnderPearlItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.UseAction;
+import net.minecraft.stats.Stats;
+import net.minecraft.util.*;
+import net.minecraft.world.World;
+import net.more.moremc.item.ModItemGroup;
+
+public class ender_ballest extends Item{
+
+    public ender_ballest(Properties p_i48487_1_) {
+        super (p_i48487_1_);
+        p_i48487_1_.tab (ModItemGroup.MOREMCTAB);
+        p_i48487_1_.stacksTo (1);
+        p_i48487_1_.durability (500);
+    }
+
+    public void crossbow(){
+
+    }
+
+    public ActionResult<ItemStack> use(World p_77659_1_, PlayerEntity p_77659_2_, Hand p_77659_3_) {
+        ItemStack itemstack = p_77659_2_.getItemInHand(p_77659_3_);
+        p_77659_1_.playSound((PlayerEntity)null, p_77659_2_.getX(), p_77659_2_.getY(), p_77659_2_.getZ(), SoundEvents.ENDER_PEARL_THROW, SoundCategory.NEUTRAL, 0.5F, 0.4F / (random.nextFloat() * 0.4F + 0.8F));
+        p_77659_2_.getCooldowns().addCooldown(this, 20);
+        if (!p_77659_1_.isClientSide) {
+            EnderPearlEntity enderpearlentity = new EnderPearlEntity(p_77659_1_, p_77659_2_);
+            enderpearlentity.setItem(itemstack);
+            enderpearlentity.shootFromRotation(p_77659_2_, p_77659_2_.xRot, p_77659_2_.yRot, 0.0F, 1.5F, 1.0F);
+            p_77659_1_.addFreshEntity(enderpearlentity);
+        }
+
+        p_77659_2_.awardStat(Stats.ITEM_USED.get(this));
+        if (!p_77659_2_.abilities.instabuild) {
+            itemstack.shrink(1);
+        }
+
+        return ActionResult.sidedSuccess(itemstack, p_77659_1_.isClientSide());
+    }
+}
